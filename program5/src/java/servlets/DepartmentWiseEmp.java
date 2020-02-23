368cc79ecceae68d137f6bf32be80e04f5fa12d2
Mohit Jariwala <42110541+moheetjari@users.noreply.github.com>
2020-02-23 17:27:19 +0530

    program5 done

---
+++ program5/src/java/servlets/DepartmentWiseEmp.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+public class DepartmentWiseEmp extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DepartmentWiseEmp</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<center><h1>Department Wise Employees</h1></center>");
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/employeedb", "root", "root");
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT E.EMPLOYEENAME,E.SALARY,D.DEPARTMENTNAME FROM EMPLOYEE E,DEPARTMENT D WHERE E.DEPARTMENTID=D.ID");
+
+                while (rs.next()) {
+                    out.println("<tr>");
+                    String EmployeeName = rs.getString(1);
+                    Integer EmployeeSalary = rs.getInt(2);
+                    String DepartmentName = rs.getString(3);
+                    out.println("<center><h1>" + DepartmentName + "</h1><h2>" + EmployeeName + " " + EmployeeSalary + "</h2></center><br>");
+                }
+            } catch (ClassNotFoundException | SQLException ex) {
+                out.println(ex);
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
